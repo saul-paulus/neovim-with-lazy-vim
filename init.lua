d5a4562262dec201b7023bdd8c24f49e3b7cd966
saul-paulus <saulpaulus17@icloude.com>
2024-02-11 12:15:00 +0000

    feat: Install colorshcem onedark for themes

--- init.lua
+++ init.lua
@@ -16,7 +16,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {}
+local plugins = {
+  {
+    "olimorris/onedarkpro.nvim", priority = 1000,
+  }
+}
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+vim.cmd("colorscheme onedark")
