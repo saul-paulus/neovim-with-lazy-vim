429e23e118573664b4533e9222cc12e071141c73
saul-paulus <saulpaulus17@icloud.com>
2026-07-30 01:30:35 +0700

    feat: add inspector client global

--- lua/core/lsp/init.lua
+++ lua/core/lsp/init.lua
@@ -1,3 +1,24 @@
+if vim.lsp and vim.lsp.get_clients then
+  local orig_get_clients = vim.lsp.get_clients
+  vim.lsp.get_clients = function(...)
+    local clients = orig_get_clients(...)
+    for _, client in ipairs(clients) do
+      if client and client.supports_method and not rawget(client, "_supports_method_patched") then
+        local orig = client.supports_method
+        rawset(client, "_supports_method_patched", true)
+        client.supports_method = function(self_or_method, ...)
+          if type(self_or_method) == "string" then
+            return orig(client, self_or_method, ...)
+          end
+          return orig(self_or_method, ...)
+        end
+      end
+    end
+    return clients
+  end
+end
+
 require("core.lsp.handlers").setup()
 require("core.lsp.mason")
+
 
